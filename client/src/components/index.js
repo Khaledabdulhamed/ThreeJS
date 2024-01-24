9842db19ec7eb4ff5a5ef7fa64fb7fd55fb035d7
Khaled Abdulhamid <khaledraedabdulhamed101@hotmail.com>
2024-01-24 16:28:58 -0500

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import Tab from "./Tab";
+import FilerPicker from "./FilerPicker";
+
+export {CustomButton, AIPicker, ColorPicker, Tab, FilerPicker};
